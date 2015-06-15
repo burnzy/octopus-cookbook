2012edb653dc7d09543a43aac8837ad0ce15a41c
Taliesin Sisson <taliesins@yahoo.com>
2015-06-15 22:58:55 +0100

    Add a recipe to create environment

---
+++ recipes/create_environment.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: octopus
+# Recipe:: register_tentacle
+#
+# Copyright 2014, Shaw Media Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# register the tentacle with octopus server
+powershell_script "create_tentacle_environment" do
+	code <<-EOH
+	Set-Alias octo "#{node['octopus']['tools']['home']}\\Octo.exe"
+	octo create-environment --name #{node.chef_environment} --ignoreIfExists --server=#{node['octopus']['api']['uri']} --apiKey=#{node['octopus']['api']['key']}
+	EOH
+end
