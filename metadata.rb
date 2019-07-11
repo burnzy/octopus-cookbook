e07798a81d219dd9407b69f1e49e18c092511647
mcharlton <mcharlton@users.noreply.github.com>
2019-07-11 14:14:22 -0500

    Bumping cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mike@michaelburns.ca'
 license          "Apache 2.0"
 description      'installs, configures and registers an octopus tentacle'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.5'
+version          '0.1.6'
 supports         "windows"
 
 depends          "windows", ">= 1.2.2"
